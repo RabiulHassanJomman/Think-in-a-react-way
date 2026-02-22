179b24fb60d6835c6b2a316b6f6a9223fc7157ca
MD Jomman <75951818+RabiulHassanJomman@users.noreply.github.com>
2026-02-22 20:28:22 +0600

    higher order component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+  state = {count: 0};
+  increamentCount = () =>{
+    this.setState((prevState) => ({count: prevState.count + 1}));
+  }
+
+  render() {
+    const {count} = this.state;
+    const {children} = this.props;
+
+    return (children(count, this.increamentCount))
+  }
+}
