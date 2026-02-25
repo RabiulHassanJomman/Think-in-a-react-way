b0f756b886a54882c24db1b0ae9f874b10eb59a2
MD Jomman <75951818+RabiulHassanJomman@users.noreply.github.com>
2026-02-25 10:10:05 +0600

    style css react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-function App() {
-  return <>
-    <p>hello world</p>
-  </>
-}
+import React from 'react'
+import Logo from './components/Logo'
+import './assets/CSS/global.css'
 
-export default App
+export default function App() {
+  return (
+    <div className='App'>
+      <h1>Learn With Sumit</h1>
+      <Logo></Logo>
+    </div>
+  )
+}
